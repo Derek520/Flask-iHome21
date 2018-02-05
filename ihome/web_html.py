09b73fb305cb52612ac48f4af529d6ea689e205d
derek_520 <18700862394@139.com>
2018-02-05 18:45:07 +0800

    生成csrf_token

--- ihome/web_html.py
+++ ihome/web_html.py
@@ -2,8 +2,8 @@
 
 # web_html.py专门处理静态文件
 
-from flask import Blueprint,current_app
-
+from flask import Blueprint,current_app,make_response
+from flask_wtf.csrf import generate_csrf
 # 创建蓝图
 web_html = Blueprint('web_html',__name__)
 
@@ -17,6 +17,8 @@ web_html = Blueprint('web_html',__name__)
 
 @web_html.route('/<re(".*"):file>')
 def index(file):
+
+
     print file
     # 如果没有传文件 访问的是127.0.0.1 就给file赋值index.html
     if not file:
@@ -26,7 +28,13 @@ def index(file):
         file = 'html/'+file
     print '-'*30
     print file
-    return current_app.send_static_file(file)
+
+    # 导入flask_wtf.csrf中的generate_csrf()
+    # generate_csrf() 可以生成csrf_token
+    csrf = generate_csrf()
+    response = make_response(current_app.send_static_file(file))
+    response.set_cookie('csrf_token',csrf,max_age=24*24)
+    return response
 
 # @web_html.route('/')
 # def index():
